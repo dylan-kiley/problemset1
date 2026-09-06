b79c23ea22651296dd42a53d07e3a6ebdc594aa8
Dylan Kiley <dylankiley@hotmail.com>
2026-09-06 13:28:54 -0400

    load & filter the entire dataset for 1995

--- ps1p1.R
+++ ps1p1.R
@@ -18,3 +18,6 @@ quantile(ce621.male$totchg, c(0, .10, .25, .50, .75, .90, 1))
 summary(ce621.female$totchg)
 sd(ce621.female$totchg)
 quantile(ce621.female$totchg, c(0, .10, .25, .50, .75, .90, 1))
+
+ce621e <- read_csv("ce621entire.csv")
+ce621e95 = filter(ce621e, year==1995)
