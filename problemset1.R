c532f4fcd0a14a0a823d91c9d7dd098895422011
Dylan Kiley <dylankiley@hotmail.com>
2026-09-06 13:19:11 -0400

    create stem leaf plot to visualize distribution

--- problemset1.R
+++ problemset1.R
@@ -4,3 +4,9 @@ ce621 <- read_csv("ce621.csv")
 summary(ce621)
 table(ce621$sex, useNA = "ifany")
 table(ce621$smoker, useNA = "ifany")
+
+ce621.male = filter(ce621, sex=="Male")
+ce621.female = filter(ce621, sex=="Female")
+
+stem(ce621.male$totchg)
+stem(ce621.female$totchg)
