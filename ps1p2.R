70b92dc3ff8957bea7f2ec8866f55a83f5e5519c
Dylan Kiley <dylankiley@hotmail.com>
2026-09-06 13:42:09 -0400

    boxplot of raw walking times stratified by number of diseases

--- ps1p2.R
+++ ps1p2.R
@@ -2,3 +2,7 @@ gait <- read_csv("621_MMC_gait.csv")
 
 summary(gait)
 str(gait)
+
+boxplot(walk_time ~ numdisease, data=gait,
+        xlab="Number of Chronic Diseases", ylab="Walk Time (seconds)",
+        main="4-Meter Walk Time by Disease Count")
