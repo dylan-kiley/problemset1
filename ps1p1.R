a6d6dd28b9f53d26524e5ef9ade337ae48d170b9
Dylan Kiley <dylankiley@hotmail.com>
2026-09-06 13:29:33 -0400

    create the categorized age variable

--- ps1p1.R
+++ ps1p1.R
@@ -21,3 +21,5 @@ quantile(ce621.female$totchg, c(0, .10, .25, .50, .75, .90, 1))
 
 ce621e <- read_csv("ce621entire.csv")
 ce621e95 = filter(ce621e, year==1995)
+
+ce621e95$agecat = cut(ce621e95$age, c(0, 50, 64, 100), right=TRUE, labels=c("<=50","51-64",">=65"))
